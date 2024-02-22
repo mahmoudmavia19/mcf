9c45d320728715391a72474731de899e30848d0a
mahmoudmavia19 <hamdymhmoud31@gamil.com>
2024-02-22 11:27:33 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,172 @@
+'use strict';
+
+/* ===== Enable Bootstrap Popover (on element  ====== */
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+/* ==== Enable Bootstrap Alert ====== */
+//var alertList = document.querySelectorAll('.alert')
+//alertList.forEach(function (alert) {
+//  new bootstrap.Alert(alert)
+//});
+
+const alertList = document.querySelectorAll('.alert')
+const alerts = [...alertList].map(element => new bootstrap.Alert(element))
+
+
+/* ===== Responsive Sidepanel ====== */
+const sidePanelToggler = document.getElementById('sidepanel-toggler'); 
+const sidePanel = document.getElementById('app-sidepanel');  
+const sidePanelDrop = document.getElementById('sidepanel-drop'); 
+const sidePanelClose = document.getElementById('sidepanel-close'); 
+
+window.addEventListener('load', function(){
+	responsiveSidePanel(); 
+});
+
+window.addEventListener('resize', function(){
+	responsiveSidePanel(); 
+});
+
+
+function responsiveSidePanel() {
+    let w = window.innerWidth;
+	if(w >= 1200) {
+	    // if larger 
+	    //console.log('larger');
+		sidePanel.classList.remove('sidepanel-hidden');
+		sidePanel.classList.add('sidepanel-visible');
+		
+	} else {
+	    // if smaller
+	    //console.log('smaller');
+	    sidePanel.classList.remove('sidepanel-visible');
+		sidePanel.classList.add('sidepanel-hidden');
+	}
+};
+
+sidePanelToggler.addEventListener('click', () => {
+	if (sidePanel.classList.contains('sidepanel-visible')) {
+		console.log('visible');
+		sidePanel.classList.remove('sidepanel-visible');
+		sidePanel.classList.add('sidepanel-hidden');
+		
+	} else {
+		console.log('hidden');
+		sidePanel.classList.remove('sidepanel-hidden');
+		sidePanel.classList.add('sidepanel-visible');
+	}
+});
+
+
+
+sidePanelClose.addEventListener('click', (e) => {
+	e.preventDefault();
+	sidePanelToggler.click();
+});
+
+sidePanelDrop.addEventListener('click', (e) => {
+	sidePanelToggler.click();
+});
+
+
+
+/* ====== Mobile search ======= */
+const searchMobileTrigger = document.querySelector('.search-mobile-trigger');
+const searchBox = document.querySelector('.app-search-box');
+
+searchMobileTrigger.addEventListener('click', () => {
+
+	searchBox.classList.toggle('is-visible');
+	
+	let searchMobileTriggerIcon = document.querySelector('.search-mobile-trigger-icon');
+	
+	if(searchMobileTriggerIcon.classList.contains('fa-magnifying-glass')) {
+		searchMobileTriggerIcon.classList.remove('fa-magnifying-glass');
+		searchMobileTriggerIcon.classList.add('fa-xmark');
+	} else {
+		searchMobileTriggerIcon.classList.remove('fa-xmark');
+		searchMobileTriggerIcon.classList.add('fa-magnifying-glass');
+	}
+	
+		
+	
+});
+
+
+
+const appHeader = {
+	render: function () {
+		return `
+        <header class="app-header fixed-top">
+            <div class="app-header-inner">
+                <div class="container-fluid py-2">
+                    <div class="app-header-content">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-auto">
+                                <a id="sidepanel-toggler" class="sidepanel-toggler d-inline-block d-xl-none" href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" role="img"><title>Menu</title><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M4 7h22M4 15h22M4 23h22"></path></svg>
+                                </a>
+                            </div><!--//col-->
+                            <div class="search-mobile-trigger d-sm-none col">
+                                <i class="search-mobile-trigger-icon fa-solid fa-magnifying-glass"></i>
+                            </div><!--//col-->
+                        </div><!--//row-->
+                    </div><!--//app-header-content-->
+                </div><!--//container-fluid-->
+            </div><!--//app-header-inner-->
+            <div id="app-sidepanel" class="app-sidepanel sidepanel-hidden">
+                <div id="sidepanel-drop" class="sidepanel-drop"></div>
+                <div class="sidepanel-inner d-flex flex-column">
+                    <a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
+                    <div class="app-branding">
+                        <a class="app-logo" href="../index.html"><img class="logo-icon me-2" src="../assets/images/app-logo.svg" alt="logo"><span class="logo-text">MCF</span></a>
+                    </div><!--//app-branding-->
+                    <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
+                        <ul class="app-menu list-unstyled accordion" id="menu-accordion">
+                            <li class="nav-item has-submenu">
+                                <a class="nav-link submenu-toggle" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-1" aria-expanded="false" aria-controls="submenu-1">
+                                    <span class="nav-icon">
+                                        <i class="fas fa-users"></i>
+                                    </span>
+                                    <span class="nav-link-text">Employee Management</span>
+                                    <span class="submenu-arrow">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/></svg>
+                                    </span><!--//submenu-arrow-->
+                                </a><!--//nav-link-->
+                                <div id="submenu-1" class="collapse submenu submenu-1" data-bs-parent="#menu-accordion">
+                                    <ul class="submenu-list list-unstyled">
+                                        <li class="submenu-item"><a class="submenu-link" href="EmployeeMangement.html">Show Employees</a></li>
+                                        <li class="submenu-item"><a class="submenu-link" href="AddEmployee.html">Add Employee</a></li>
+                                        <li class="submenu-item"><a class="submenu-link" href="SendNotification.html">Send Notification</a></li>
+                                    </ul>
+                                </div>
+                            </li><!--//nav-item-->
+                            <li class="nav-item has-submenu">
+                                <a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-2" aria-expanded="false" aria-controls="submenu-2">
+                                    <span class="nav-icon">
+                                        <i class="fas fa-building"></i>
+                                    </span>
+                                    <span class="nav-link-text">Department Management</span>
+                                    <span class="submenu-arrow">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/></svg>
+                                    </span><!--//submenu-arrow-->
+                                </a><!--//nav-link-->
+                                <div id="submenu-2" class="collapse submenu submenu-1" data-bs-parent="#menu-accordion">
+                                    <ul class="submenu-list list-unstyled">
+                                        <li class="submenu-item"><a class="submenu-link" href="DepartmentManagement.html">Show Department</a></li>
+                                        <li class="submenu-item"><a class="submenu-link" href="AddDepartment.html">Add Department</a></li>
+                                    </ul>
+                                </div>
+                            </li><!--//nav-item-->
+                        </ul><!--//app-menu-->
+                    </nav><!--//app-nav-->
+                </div><!--//sidepanel-inner-->
+            </div><!--//app-sidepanel-->
+        </header><!--//app-header-->
+        `;
+	}
+};
+
+// Render the header by appending its HTML content to a specific element in your document
+document.getElementById('your-header-container-id').innerHTML = appHeader.render();
